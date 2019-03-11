c24db03485fbdd07eb801a858eb9d298bf3be71c
Quentin <Coulis1990@gmail.com>
2019-03-11 16:55:06 +0100

    I still haven't stopped yet

--- app/src/main/java/com/example/mangacomiclist/Main2Activity.kt
+++ app/src/main/java/com/example/mangacomiclist/Main2Activity.kt
@@ -2,6 +2,8 @@ package com.example.mangacomiclist
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import kotlin.random.Random
 
 class Main2Activity : AppCompatActivity() {
 
@@ -12,11 +14,18 @@ class Main2Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+        showRandomNumber()
     }
 
     fun showRandomNumber() {
         val count =  intent.getIntExtra(TOTAL_COUNT, 0)
-        val random = Random()
         var randomInt = 0
+
+        if (count > 0) {
+            randomInt = Random.nextInt(count + 1)
+        }
+
+        findViewById<TextView>(R.id.random_number).text = Integer.toString(randomInt)
+        findViewById<TextView>(R.id.textView3).text = getString(R.string.num_0_val, count)
     }
 }
